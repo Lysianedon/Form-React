3877e032aa1850bdfd63d0f1d53bf594bcf5c19f
lysiandon <don.lysiane@gmail.com>
2022-01-31 11:10:20 +0100

    finished confirmation page

---
+++ src/ConfirmationPage/ConfirmationPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ConfirmationPage extends React.Component {
+
+    render(){
+
+        return (
+
+            <div className="row col-6 mx-auto mt-5 p-4 submitted">
+                <h1 className="mx-auto">Form submitted</h1>
+            </div>
+        )
+    }
+}
+
+
+export default ConfirmationPage;
