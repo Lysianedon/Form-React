20652fb278b22cf496dc3fc86d7e907072ecee2b
lysiandon <don.lysiane@gmail.com>
2022-02-06 01:30:49 +0100

    added error message

--- src/ConfirmationPage/ConfirmationPage.js
+++ src/ConfirmationPage/ConfirmationPage.js
@@ -1,14 +1,54 @@
 import React from "react";
+import App from "../App";
+
 
 class ConfirmationPage extends React.Component {
 
+
+    constructor(){
+        super();
+
+        this.state = {
+
+            errorMessage : "hidden",
+            display : "block",
+        }
+        //Bind functions :
+        this.errorMessage = this.errorMessage.bind(this);
+    }
+
+    errorMessage (){
+
+        setTimeout(() => {
+            this.setState({display : "none"});
+
+        }, 2000);
+
+    }
     render(){
 
         return (
 
-            <div className="row col-6 mx-auto mt-5 p-4 submitted">
-                <h1 className="mx-auto">Form submitted</h1>
+            <div>
+
+            {
+                this.props.password.length >= 6 && this.props.emailInput.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) ?
+                
+                ( <div className="row col-6 mx-auto mt-5 p-4 submitted">
+                        <h1 className="mx-auto">Form submitted</h1>
+                </div>)
+                :
+                (
+                    <> 
+                    {this.errorMessage()}
+                    <p className="error" style={{display : this.state.display}}>Erreur : champs invalides !</p>
+                    <App/>
+                    </>
+                )
+            }
             </div>
+
+
         )
     }
 }
